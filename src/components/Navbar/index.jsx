252b085a105906e31e85cca22b4bb7ddad689890
Urvish Patel <urvish.xyz@gmail.com>
2024-02-16 23:13:04 +0530

    Read more button added

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -10,22 +10,22 @@ const categories = [
   },
   {
     name: "Science",
-    id: 2,
+    id: 10,
     apiName: "science",
   },
   {
     name: "Entertainment",
-    id: 3,
+    id: 9,
     apiName: "entertainment",
   },
   {
     name: "Sports",
-    id: 4,
+    id: 8,
     apiName: "sports",
   },
   {
     name: "Politics",
-    id: 5,
+    id: 7,
     apiName: "politics",
   },
   {
@@ -35,22 +35,22 @@ const categories = [
   },
   {
     name: "Technology",
-    id: 7,
+    id: 5,
     apiName: "technology",
   },
   {
     name: "World",
-    id: 9,
+    id: 4,
     apiName: "world",
   },
   {
     name: "Education",
-    id: 11,
+    id: 3,
     apiName: "education",
   },
   {
     name: "Travel",
-    id: 12,
+    id: 2,
     apiName: "travel",
   },
 ];
@@ -73,12 +73,22 @@ const Navbar = ({ selectedCategory, setSelectedCategory, setMyData }) => {
   };
 
   return (
-    <header className="justify- fixed z-30 flex h-auto w-full items-center justify-around bg-gray-500 px-3 py-3 backdrop-blur-sm">
+    <header className="fixed z-30 flex items-center justify-around w-full h-auto px-3 py-3 bg-gray-500 justify- backdrop-blur-sm">
       <div className="flex justify-center px-2 text-3xl font-semibold text-white">
         XYZ News
       </div>
       <nav className="flex">
-        <ul className="justify-content-between flex flex-wrap justify-center gap-3 max-lg:hidden md:gap-5">
+        <ul className="flex flex-wrap justify-center gap-3 justify-content-between max-lg:hidden md:gap-5">
+          {categories.map((category) => (
+            <NavCategories
+              selectedCategory={selectedCategory}
+              category={category}
+              key={category.id}
+              onClick={handleCategoryClick}
+            />
+          ))}
+        </ul>
+        <ul className="justify-content-between absolute left-0 mt-5 hidden w-[100vw] flex-col items-center justify-center gap-3 bg-gray-500 max-lg:flex md:gap-5">
           {categories.map((category) => (
             <NavCategories
               selectedCategory={selectedCategory}
--- src/components/NewsSection/index.jsx
+++ src/components/NewsSection/index.jsx
@@ -4,10 +4,10 @@ const NewsSection = ({ mydata }) => {
   return (
     <div className="grid w-full grid-cols-1 gap-5 px-3 pt-14 sm:grid-cols-2 lg:grid-cols-2">
       {mydata.map((x) => (
-        <div className="border-clr mt-12 w-full overflow-hidden rounded-xl ">
+        <div className="w-full mt-12 overflow-hidden border-clr rounded-xl ">
           <div className="img_news">
             <img
-              className="transition delay-150 ease-in-out hover:scale-105"
+              className="transition ease-in-out delay-150 hover:scale-105"
               src={x.imageUrl}
               alt=""
             />
@@ -22,6 +22,13 @@ const NewsSection = ({ mydata }) => {
                 {x.time} on {x.date}
               </p>
             </div>
+          </div>
+          <div className="flex justify-end w-full px-2 pb-2 font-bold">
+            <button className="px-2 bg-gray-300 rounded-md" type="button">
+              <a href={x.readMoreUrl} alt="linkofurl">
+                Read More {'>'}
+              </a>
+            </button>
           </div>
         </div>
       ))}
